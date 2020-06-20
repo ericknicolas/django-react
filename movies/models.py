78d9d31c869e43735eee8374ca85364d392b8293
erick <ericknicolas@gmail.com>
2020-06-20 12:40:33 +0200

    added serializer for movies and ratings

--- movies/models.py
+++ movies/models.py
@@ -6,13 +6,28 @@ class Movie(models.Model):
     title = models.CharField(max_length=200)
     description = models.CharField(max_length=400)
 
+    def num_of_ratings(self):
+        return len(Rating.objects.filter(movie=self))
+    
+    def avg_rating(self):
+        sum = 0
+        ratings = Rating.objects.filter(movie=self)
+
+        for rating in ratings:
+            sum += rating.stars
+        
+        if len(ratings) > 0:
+            return sum / len(ratings)
+        else:
+            return 0
+
     def __str__(self):
         return self.title
 
 class Rating(models.Model):
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    starts = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    stars = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
 
     def __str__(self):
         return self.movie.title
